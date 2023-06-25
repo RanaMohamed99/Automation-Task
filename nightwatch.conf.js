6122bc72a841ec215b6c9599f20d32e49bce062e
Rana Mohamed <88551219+RanaMohamed99@users.noreply.github.com>
2023-06-26 00:32:20 +0300

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -68,10 +68,10 @@ module.exports = {
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
-            //'--no-sandbox',
+              '--no-sandbox',
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
-            //'--headless'
+              '--headless'
           ]
         }
       },
