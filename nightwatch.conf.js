f0ac4037f4323a01520fc4015592cd725d6bf449
Rana Mohamed <88551219+RanaMohamed99@users.noreply.github.com>
2023-07-18 00:23:30 +0300

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -54,7 +54,7 @@ module.exports = {
       
       webdriver: {
         start_process: true,
-        server_path: ''
+        server_path: './lib/drivers/chromedriver.exe'
       },
       
     },
